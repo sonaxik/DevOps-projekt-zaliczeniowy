a60ade54911930f4146f0f7f3f5ae505557d8857
sonaxik <dawid.szarlej@op.pl>
2026-02-09 22:40:46 +0100

    Prosta aplikacja z endpointami. (Resolves #2)

--- Program.cs
+++ Program.cs
@@ -1,23 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// Endpoint g³ówny (/)
+app.MapGet("/", () => "Witaj w TicketManagement API! PrzejdŸ do /products aby zobaczyæ bilety.");
 
-app.MapControllers();
+// Endpoint produktów (/products) 
+var tickets = new List<Ticket>
+{
+    new Ticket(1, "Koncert Metallica", "2025-06-15", 350.00m),
+    new Ticket(2, "Mecz Polska - Niemcy", "2025-09-10", 120.50m),
+    new Ticket(3, "Teatr: Hamlet", "2025-11-05", 80.00m),
+    new Ticket(4, "Kino: Diuna 3", "2025-07-20", 35.00m)
+};
+
+app.MapGet("/products", () => tickets)
+   .WithName("GetTickets")
+   .WithOpenApi();
 
 app.Run();
+record Ticket(int Id, string Name, string Date, decimal Price);
